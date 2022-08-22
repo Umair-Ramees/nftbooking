1434cbb7f810ea7186490972d0c32edb721fd484
Nabi <46531267+nabi-topdev@users.noreply.github.com>
2022-08-22 23:55:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
+ 
 import Home from "./components/Home/Index";
 import Road from "./components/Road/Index";
 import Lounge from "./components/Lounge/Index";
